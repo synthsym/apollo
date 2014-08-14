4552f1bf20dc2313390820384e61aec7da29ec48
Adam Jacobus <snoopyk921@gmail.com>
2014-08-14 18:55:00 +1000

    Removing School resource

--- spec/factories/admin_schools.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :admin_school, :class => 'Admin::School' do
-    name "MyString"
-    acronym "MyString"
-  end
-end
